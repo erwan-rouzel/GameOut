b4a20a12058e3d892732cdb3d4d00acd53f3d62b
erwan-rouzel <ewn40rzl>
2016-02-03 13:35:54 +0100

    Correction git

--- app/src/main/java/fr/ecp/sio/gameout/model/PlayerState.java
+++
@@ -1,9 +0,0 @@
-package fr.ecp.sio.gameout.model;
-
-/**
- * Created by erwan on 14/11/2015.
- */
-public class PlayerState {
-    public final static byte ACTIVE = 1;
-    public final static byte INACTIVE = 0;
-}
