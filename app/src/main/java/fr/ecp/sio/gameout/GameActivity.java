cc4c48a9215e5160773255431484eb2a1965e43f
odeste <olivier.deste@wanadoo.fr>
2016-02-03 10:24:10 +0100

    Passage de ActionBarActivity à Activity

--- app/src/main/java/fr/ecp/sio/gameout/GameActivity.java
+++ app/src/main/java/fr/ecp/sio/gameout/GameActivity.java
@@ -22,10 +22,10 @@
  */
 package fr.ecp.sio.gameout;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -61,7 +61,7 @@ import fr.ecp.sio.gameout.remote.helper.SyncStateService;
  * - Olivier Desté : intial idea and Android dev
  *
  */
-public class GameActivity extends ActionBarActivity implements
+public class GameActivity extends Activity implements
         ConnectionCallbacks, OnConnectionFailedListener, LocationListener {
     protected char balTraffic = 'N';
     //private   BallThread balThread = null;
