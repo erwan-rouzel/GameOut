c081ccd20927bdf84bb64b9681447cf09342c44f
abdelhafiz23 <abdelhafizyayacisse@gmail.com>
2016-02-24 15:47:57 +0100

    parametrage pubs

--- app/src/main/java/fr/ecp/sio/gameout/salon/ParamActivity.java
+++ app/src/main/java/fr/ecp/sio/gameout/salon/ParamActivity.java
@@ -8,6 +8,8 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.util.Arrays;
+
 import fr.ecp.sio.gameout.R;
 import fr.ecp.sio.gameout.utils.SharedPreferencesUtils;
 
@@ -16,13 +18,16 @@ import fr.ecp.sio.gameout.utils.SharedPreferencesUtils;
  */
 public class ParamActivity extends ActionBarActivity {
     public static String nomServeur, niveauPub, lissageParam;
+    Spinner spinner1;
+    Spinner spinner2;
+    Spinner spinner3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_param);
-        Spinner spinner1 = (Spinner) findViewById(R.id.spinner1);
-        Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
-        Spinner spinner3 = (Spinner) findViewById(R.id.spinner3);
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+        spinner2 = (Spinner) findViewById(R.id.spinner2);
+        spinner3 = (Spinner) findViewById(R.id.spinner3);
 
         // Create an Adapter that holds a list
         ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this, R.array.ads_level, R.layout.dropdown_item);
@@ -32,18 +37,25 @@ public class ParamActivity extends ActionBarActivity {
         spinner2.setAdapter(adapter2);
         spinner3.setAdapter(adapter3);
 
+            spinner1.setSelection(Integer.parseInt(SharedPreferencesUtils.readSharedSetting(getApplicationContext(), "AD_LEVEL_POS", "0")));
+            spinner2.setSelection(Integer.parseInt(SharedPreferencesUtils.readSharedSetting(getApplicationContext(), "SERVEUR_NAME_POS", "0")));
+            spinner3.setSelection(Integer.parseInt(SharedPreferencesUtils.readSharedSetting(getApplicationContext(), "LISSAGE_PARAM_POS", "0")));
+
+
         spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             public void onItemSelected(AdapterView<?> parent, View view,
                                        int pos, long id) {
 
                 niveauPub = parent.getItemAtPosition(pos).toString();
                 Toast.makeText(parent.getContext(), "The pub level is " + niveauPub, Toast.LENGTH_LONG).show();
-                SharedPreferencesUtils.saveSharedSetting(getApplicationContext(),"AD_LEVEL", niveauPub);
+                SharedPreferencesUtils.saveSharedSetting(getApplicationContext(), "AD_LEVEL", niveauPub);
+                SharedPreferencesUtils.saveSharedSetting(getApplicationContext(), "AD_LEVEL_POS", String.valueOf(pos));
 
             }
 
             public void onNothingSelected(AdapterView<?> parent) {
             }
+
         });
 
         spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -53,6 +65,7 @@ public class ParamActivity extends ActionBarActivity {
                 nomServeur = parent.getItemAtPosition(pos).toString();
                 Toast.makeText(parent.getContext(), "The server name is " + nomServeur, Toast.LENGTH_LONG).show();
                 SharedPreferencesUtils.saveSharedSetting(getApplicationContext(), "SERVEUR_NAME", nomServeur);
+                SharedPreferencesUtils.saveSharedSetting(getApplicationContext(), "SERVEUR_NAME_POS", String.valueOf(pos));
 
             }
 
@@ -67,11 +80,26 @@ public class ParamActivity extends ActionBarActivity {
                 lissageParam = parent.getItemAtPosition(pos).toString();
                 Toast.makeText(parent.getContext(), "The lissage param is " + lissageParam, Toast.LENGTH_LONG).show();
                 SharedPreferencesUtils.saveSharedSetting(getApplicationContext(), "LISSAGE_PARAM", lissageParam);
+                SharedPreferencesUtils.saveSharedSetting(getApplicationContext(), "LISSAGE_PARAM_POS", String.valueOf(pos));
 
             }
 
             public void onNothingSelected(AdapterView<?> parent) {
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        this.finish();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SharedPreferencesUtils.saveSharedSetting(getApplicationContext(), "AD_LEVEL", niveauPub);
+        SharedPreferencesUtils.saveSharedSetting(getApplicationContext(), "SERVEUR_NAME", nomServeur);
+        SharedPreferencesUtils.saveSharedSetting(getApplicationContext(), "LISSAGE_PARAM", lissageParam);
     }
 }
